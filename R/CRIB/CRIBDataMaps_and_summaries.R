7dde666d4ffa50836033de82414529448e0dc5c8
Stortini <Christine.Stortini@dfo-mpo.gc.ca>
2026-02-24 11:20:34 -0500

    Back-calculated Time of Emergence years and added Greenland Hablibut

--- R/CRIB/CRIBDataMaps_and_summaries.R
+++ R/CRIB/CRIBDataMaps_and_summaries.R
@@ -52,7 +52,7 @@ CAMAP<-ggplot() +
   geom_sf(data = EEZ, color="navy", linetype = "dashed", size = 1.2) +
   geom_sf(data = NAFO, color="darkgrey", fill = NA) +
   geom_sf(data = land, fill="cornsilk") +
-  scale_fill_manual(values = region_colours)+
+  #scale_fill_manual(values = region_colours)+
   #scale_fill_manual(name = " ", values = c("#E41A1C","#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628","#F781BF", "#999999")) +
   labs(title="")+
   xlim(-72.5, -48) + ylim(39.355, 50)+
@@ -60,34 +60,39 @@ CAMAP<-ggplot() +
   theme(axis.text = element_text(angle = 0, vjust = 0.2, hjust=1,size=8,family="serif"))
 CAMAP
 
-### Add CRIB data for Halibut ###
+### Add CRIB data ###
 require("readr")
 url <- "https://api-proxy.edh-cde.dfo-mpo.gc.ca/catalogue/records/264692e5-7b51-4ab9-bb1f-da65f6fc0875/attachments/EN_ClimateRiskIndex_Spatial-CanEEZ_145Spp.csv"
 crib.df <- read_csv(url)
 head(crib.df)
 #write.csv(crib.df, "Data/CRIB/cribspeciesdata.csv") #file too large
 
+#################################################################################################
+##################################### HALIBUT ###################################################
+#################################################################################################
 #select only halibut data
 halcrib <- crib.df[crib.df$`common name` == "Atlantic Halibut",]
 head(halcrib)
 str(halcrib)
-write.csv(halcrib, "Data/CRIB/crib_halibut.csv")
+#write.csv(halcrib, "Data/CRIB/crib_halibut.csv")
+halcrib<-read.csv("Data/CRIB/crib_halibut.csv")
+halcrib$ToE.year<-2015+(-log(halcrib$E.Time.of.climate.emergence)/0.033) #calculate raw ToE's from standardized
 
 # Convert dataframe to an `sf` object
 library(sf)
 library(terra)
 library(gstat)
 halv<-halcrib[,c("longitude","latitude","Vulnerability")]
-haltsm<-halcrib[,c("longitude","latitude","S Thermal safety margin")]
-haltoe<-halcrib[,c("longitude","latitude","E Time of climate emergence")]
-halcv<-halcrib[,c("longitude","latitude","E Climate velocity")]
-halthv<-halcrib[,c("longitude","latitude","AC Thermal habitat availability")]
+haltsm<-halcrib[,c("longitude","latitude","S.Thermal safety.margin")]
+haltoe<-halcrib[,c("longitude","latitude","ToE.year")]
+halcv<-halcrib[,c("longitude","latitude","E.Climate.velocity")]
+halthv<-halcrib[,c("longitude","latitude","AC.Thermal.habitat.availability")]
 # Convert to a spatial object
-spatial_points <- vect(halthv, geom = c("longitude", "latitude"), crs = "WGS84")
+spatial_points <- vect(haltoe, geom = c("longitude", "latitude"), crs = "WGS84")
 # Create an empty raster (set resolution and extent as needed)
 r <- rast(extent=spatial_points, resolution = 0.25, crs = "WGS84") # Adjust resolution
 # Rasterize the points into a grid
-raster_data <- rasterize(spatial_points, r, field = "AC Thermal habitat availability", fun = mean)
+raster_data <- rasterize(spatial_points, r, field = "ToE.year", fun = mean)
 #plot(raster_data)
 
 #add halibut vulnerability rasters to NAFO map
@@ -95,16 +100,135 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(viridis) 
 raster_df<-as.data.frame(raster_data, xy=TRUE)
-colnames(raster_df) <- c("longitude", "latitude", "AC Thermal habitat availability") 
+colnames(raster_df) <- c("longitude", "latitude", "ToE.year") 
 VMAP <- ggplot() +
-  geom_raster(data = raster_df, aes(x = longitude, y = latitude, fill = `AC Thermal habitat availability`)) +
+  geom_raster(data = raster_df, aes(x = longitude, y = latitude, fill = `ToE.year`)) +
+  geom_sf(data = All_region_df, fill = NA) +
+  geom_sf(data = Hague, color = "navy") +
+  geom_sf(data = EEZ, color = "navy", linetype = "dashed", size = 1.2) +
+  geom_sf(data = NAFO, color = "darkgrey", size=0.9, fill = NA) +
+  geom_sf(data = land, fill = "cornsilk") +
+  scale_fill_viridis_c(option = "D", name = "Time of Climate Emergence") +
+  xlim(-72.5, -48) + ylim(39.355, 50) +
+  theme_bw() +
+  theme(axis.text = element_text(angle = 0, vjust = 0.2, hjust = 1, size = 8, family = "serif")) +
+  labs(title = "Time of Climate Emergence", x = "Longitude", y = "Latitude", color = "Time of Climate Emergence")
+print(VMAP) 
+
+### Calculate average + SD for each indicator in zones 4X, 4VW, and 3KL:###
+library(sf)      # For spatial data handling
+library(dplyr)   # For summarizing grouped data
+library(here)
+# Convert halcrib data frame to a spatial object
+halcrib <- st_as_sf(halcrib, coords = c("longitude", "latitude"), crs = "WGS84")
+# Reproject both datasets to align their CRS (e.g., EPSG:4326)
+halcrib <- st_transform(halcrib, crs = st_crs(NAFO))
+# Perform a spatial join to add the NAFO zone info to each point in halcrib
+halcrib_with_zones <- st_join(halcrib, NAFO, na.rm=TRUE)
+
+# Check the first few rows of the resulting dataset
+head(halcrib_with_zones)
+names(halcrib_with_zones)
+unique(halcrib_with_zones$ZONE)
+# Remove rows where NAFO_ID or ZONE are NA
+halcrib_with_zones_clean <- halcrib_with_zones[] %>%
+  filter(!is.na(NAFO_ID), !is.na(ZONE))
+unique(halcrib_with_zones_clean$ZONE)
+head(halcrib_with_zones_clean)
+
+#calculate raw ToE's from standardized
+halcrib_with_zones_clean$ToE.year<-2015+(-log(halcrib_with_zones_clean$E.Time.of.climate.emergence)/0.033)
+
+# Group halcrib_with_zones by NAFO zone and calculate summary statistics
+summary_stats <- halcrib_with_zones_clean %>%
+  group_by(NAFO_zone = ZONE) %>%
+  summarise(
+    mean_vulnerability = mean(Vulnerability, na.rm = TRUE),
+    sd_vulnerability = sd(Vulnerability, na.rm = TRUE),
+    mean_s_thermal_safety_margin = mean(`S.Thermal.safety.margin`, na.rm = TRUE),
+    sd_s_thermal_safety_margin = sd(`S.Thermal.safety.margin`, na.rm = TRUE),
+    mean_e_climate_velocity = mean(`E.Climate.velocity`, na.rm = TRUE),
+    sd_e_climate_velocity = sd(`E.Climate.velocity`, na.rm = TRUE),
+    mean_yr_climate_velocity = mean(`E.Climate.velocity`, na.rm = TRUE),
+    sd_yr_climate_velocity = sd(`E.Climate.velocity`, na.rm = TRUE),
+    mean_e_time_climate_emergence = mean(`ToE.year`, na.rm = TRUE),
+    sd_e_time_climate_emergence = sd(`ToE.year`, na.rm = TRUE),
+    mean_ac_thermal_habitat_availability = mean(`AC.Thermal.habitat.availability`, na.rm = TRUE),
+    sd_ac_thermal_habitat_availability = sd(`AC.Thermal.habitat.availability`, na.rm = TRUE)
+  )
+
+# View the resulting summarized data
+print(summary_stats)
+head(summary_stats)
+# Remove geometry column
+non_spatial_data <- st_drop_geometry(summary_stats)
+head(non_spatial_data)
+df<-as.data.frame(non_spatial_data)
+write.csv(df, "CRIB results/crib_halibut_byNAFO.csv", row.names = FALSE)
+
+# Group halcrib_with_zones by NAFO zone and calculate summary risks
+most_frequent_risks <- halcrib_with_zones_clean %>%
+  group_by(ZONE) %>%  # Group by NAFO zone
+  summarise(
+    most_frequent_overall_risk = names(sort(table(`Overall Risk`), decreasing = TRUE)[1]),
+    most_frequent_s_thermal_safety_margin_risk = names(sort(table(`S Thermal safety margin risk`), decreasing = TRUE)[1]),
+    most_frequent_e_climate_velocity_risk = names(sort(table(`E Climate velocity risk`), decreasing = TRUE)[1]),
+    most_frequent_e_time_of_climate_emergence_risk = names(sort(table(`E Time of climate emergence risk`), decreasing = TRUE)[1]),
+    most_frequent_ac_thermal_habitat_availability_risk = names(sort(table(`AC Thermal habitat availability risk`), decreasing = TRUE)[1]),
+    .groups = "drop"  # Ungroup after summarizing
+  )
+print(most_frequent_risks)
+
+# Remove geometry column
+non_spatial_data <- st_drop_geometry(most_frequent_risks)
+head(non_spatial_data)
+non_spatial_data<-as.data.frame(non_spatial_data)
+write.csv(non_spatial_data, "CRIB results/crib_risk_halibut_byNAFO.csv", row.names = FALSE)
+
+#################################################################################################
+##################################### Greenland halibut ###################################################
+#################################################################################################
+#select Arctic cod data
+unique(crib.df$`common name`)
+halcrib <- crib.df[crib.df$`common name` == "Greenland Halibut",]
+head(halcrib)
+str(halcrib)
+#write.csv(halcrib, "Data/CRIB/crib_greenland_halibut.csv")
+halcrib<-read.csv("Data/CRIB/crib_greenland_halibut.csv")
+halcrib$ToE.year<-2015+(-log(halcrib$E.Time.of.climate.emergence)/0.033) #calculate raw ToE's from standardized
+
+# Convert dataframe to an `sf` object
+library(sf)
+library(terra)
+library(gstat)
+halv<-halcrib[,c("longitude","latitude","Vulnerability")]
+haltsm<-halcrib[,c("longitude","latitude","S.Thermal.safety.margin")]
+haltoe<-halcrib[,c("longitude","latitude","ToE.year")]
+halcv<-halcrib[,c("longitude","latitude","E.Climate.velocity")]
+halthv<-halcrib[,c("longitude","latitude","AC.Thermal.habitat.availability")]
+# Convert to a spatial object
+spatial_points <- vect(halthv, geom = c("longitude", "latitude"), crs = "WGS84")
+# Create an empty raster (set resolution and extent as needed)
+r <- rast(extent=spatial_points, resolution = 0.25, crs = "WGS84") # Adjust resolution
+# Rasterize the points into a grid
+raster_data <- rasterize(spatial_points, r, field = "AC.Thermal.habitat.availability", fun = mean)
+#plot(raster_data)
+
+#add halibut vulnerability rasters to NAFO map
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(viridis) 
+raster_df<-as.data.frame(raster_data, xy=TRUE)
+colnames(raster_df) <- c("longitude", "latitude", "AC.Thermal.habitat.availability") 
+VMAP <- ggplot() +
+  geom_raster(data = raster_df, aes(x = longitude, y = latitude, fill = `AC.Thermal.habitat.availability`)) +
   geom_sf(data = All_region_df, fill = NA) +
   geom_sf(data = Hague, color = "navy") +
   geom_sf(data = EEZ, color = "navy", linetype = "dashed", size = 1.2) +
   geom_sf(data = NAFO, color = "darkgrey", size=0.9, fill = NA) +
   geom_sf(data = land, fill = "cornsilk") +
   scale_fill_viridis_c(option = "D", name = "Thermal Habitat Variability") +
-  xlim(-72.5, -48) + ylim(39.355, 50) +
+  xlim(-72.5, -45) + ylim(39, 60) +
   theme_bw() +
   theme(axis.text = element_text(angle = 0, vjust = 0.2, hjust = 1, size = 8, family = "serif")) +
   labs(title = "Thermal Habitat Variability", x = "Longitude", y = "Latitude", color = "Thermal Habitat Variability")
@@ -126,13 +250,13 @@ head(halcrib_with_zones)
 names(halcrib_with_zones)
 unique(halcrib_with_zones$ZONE)
 # Remove rows where NAFO_ID or ZONE are NA
-halcrib_with_zones_clean <- halcrib_with_zones %>%
+halcrib_with_zones_clean <- halcrib_with_zones[] %>%
   filter(!is.na(NAFO_ID), !is.na(ZONE))
-#test:
-ggplot() +
-  +     geom_sf(data = NAFO, fill = "lightgray", color = "black") +  # Plot NAFO polygons
-  +     geom_sf(data = halcrib_with_zones_clean, color = "red", size = 2) + # Plot unmatched points
-  +     labs(title = "Unmatched Points")
+unique(halcrib_with_zones_clean$ZONE)
+head(halcrib_with_zones_clean)
+
+#calculate raw ToE's from standardized
+halcrib_with_zones_clean$ToE.year<-2015+(-log(halcrib_with_zones_clean$E.Time.of.climate.emergence)/0.033)
 
 # Group halcrib_with_zones by NAFO zone and calculate summary statistics
 summary_stats <- halcrib_with_zones_clean %>%
@@ -140,32 +264,36 @@ summary_stats <- halcrib_with_zones_clean %>%
   summarise(
     mean_vulnerability = mean(Vulnerability, na.rm = TRUE),
     sd_vulnerability = sd(Vulnerability, na.rm = TRUE),
-    mean_s_thermal_safety_margin = mean(`S Thermal safety margin`, na.rm = TRUE),
-    sd_s_thermal_safety_margin = sd(`S Thermal safety margin`, na.rm = TRUE),
-    mean_e_climate_velocity = mean(`E Climate velocity`, na.rm = TRUE),
-    sd_e_climate_velocity = sd(`E Climate velocity`, na.rm = TRUE),
-    mean_e_time_climate_emergence = mean(`E Time of climate emergence`, na.rm = TRUE),
-    sd_e_time_climate_emergence = sd(`E Time of climate emergence`, na.rm = TRUE),
-    mean_ac_thermal_habitat_availability = mean(`AC Thermal habitat availability`, na.rm = TRUE),
-    sd_ac_thermal_habitat_availability = sd(`AC Thermal habitat availability`, na.rm = TRUE)
+    mean_s_thermal_safety_margin = mean(`S.Thermal.safety.margin`, na.rm = TRUE),
+    sd_s_thermal_safety_margin = sd(`S.Thermal.safety.margin`, na.rm = TRUE),
+    mean_e_climate_velocity = mean(`E.Climate.velocity`, na.rm = TRUE),
+    sd_e_climate_velocity = sd(`E.Climate.velocity`, na.rm = TRUE),
+    mean_yr_climate_velocity = mean(`E.Climate.velocity`, na.rm = TRUE),
+    sd_yr_climate_velocity = sd(`E.Climate.velocity`, na.rm = TRUE),
+    mean_e_time_climate_emergence = mean(`ToE.year`, na.rm = TRUE),
+    sd_e_time_climate_emergence = sd(`ToE.year`, na.rm = TRUE),
+    mean_ac_thermal_habitat_availability = mean(`AC.Thermal.habitat.availability`, na.rm = TRUE),
+    sd_ac_thermal_habitat_availability = sd(`AC.Thermal.habitat.availability`, na.rm = TRUE)
   )
 
 # View the resulting summarized data
 print(summary_stats)
+head(summary_stats)
 # Remove geometry column
 non_spatial_data <- st_drop_geometry(summary_stats)
 head(non_spatial_data)
-write.csv(non_spatial_data, "CRIB results/crib_halibut_byNAFO.csv", row.names = FALSE)
+df<-as.data.frame(non_spatial_data)
+write.csv(df, "CRIB results/crib_GreenlandHalibut_byNAFO.csv", row.names = FALSE)
 
 # Group halcrib_with_zones by NAFO zone and calculate summary risks
 most_frequent_risks <- halcrib_with_zones_clean %>%
   group_by(ZONE) %>%  # Group by NAFO zone
   summarise(
-    most_frequent_overall_risk = names(sort(table(`Overall Risk`), decreasing = TRUE)[1]),
-    most_frequent_s_thermal_safety_margin_risk = names(sort(table(`S Thermal safety margin risk`), decreasing = TRUE)[1]),
-    most_frequent_e_climate_velocity_risk = names(sort(table(`E Climate velocity risk`), decreasing = TRUE)[1]),
-    most_frequent_e_time_of_climate_emergence_risk = names(sort(table(`E Time of climate emergence risk`), decreasing = TRUE)[1]),
-    most_frequent_ac_thermal_habitat_availability_risk = names(sort(table(`AC Thermal habitat availability risk`), decreasing = TRUE)[1]),
+    most_frequent_overall_risk = names(sort(table(`Overall.Risk`), decreasing = TRUE)[1]),
+    most_frequent_s_thermal_safety_margin_risk = names(sort(table(`S.Thermal.safety.margin.risk`), decreasing = TRUE)[1]),
+    most_frequent_e_climate_velocity_risk = names(sort(table(`E.Climate.velocity.risk`), decreasing = TRUE)[1]),
+    most_frequent_e_time_of_climate_emergence_risk = names(sort(table(`E.Time.of.climate.emergence.risk`), decreasing = TRUE)[1]),
+    most_frequent_ac_thermal_habitat_availability_risk = names(sort(table(`AC.Thermal.habitat.availability.risk`), decreasing = TRUE)[1]),
     .groups = "drop"  # Ungroup after summarizing
   )
 print(most_frequent_risks)
@@ -174,4 +302,4 @@ print(most_frequent_risks)
 non_spatial_data <- st_drop_geometry(most_frequent_risks)
 head(non_spatial_data)
 non_spatial_data<-as.data.frame(non_spatial_data)
-write.csv(non_spatial_data, "CRIB results/crib_risk_halibut_byNAFO.csv", row.names = FALSE)
+write.csv(non_spatial_data, "CRIB results/crib_risk_GreenlandHalibut_byNAFO.csv", row.names = FALSE)
